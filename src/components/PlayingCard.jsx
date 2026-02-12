74c38048657b64f3028e3908ee78696e516890fb
David Hays <davidhays@Mac.home.local>
2026-02-12 15:55:46 -0700

    Fix deployment: remove react-card-flip dependency

--- src/components/PlayingCard.jsx
+++ src/components/PlayingCard.jsx
@@ -1,36 +1,26 @@
-import React, {  } from 'react'
+import React from 'react';
 import { Card } from 'antd';
-// import ReactCardFlip from 'react-card-flip';
 
-
-export default function PlayingCard() {
-  // const [front, setFront] = useState(false);
-
+export default function PlayingCard({ suit, rank }) {
+  // This component is currently not in use
+  // To implement card flip functionality, consider using CSS animations
+  // or a React 19 compatible flip library
+  
   return (
-    // <ReactCardFlip isFlipped={!front} flipDirection="horizontal">
-    //   <Card
-    //     styles={{
-    //       body: {
-    //         padding: 0,
-    //       }
-    //     }}
-    //     hoverable
-    //     onClick={() => setFront(!front)}
-    //   >
-    //     <img src={`/svg_playing_cards/fronts/${suit}_${rank}.svg`} alt={`${rank} of ${suit}`} style={{ width: '100%', height: 'auto' }} />
-    //   </Card>
-    //   <Card
-    //     styles={{
-    //       body: {
-    //         padding: 0,
-    //       }
-    //     }}
-    //     hoverable
-    //     onClick={() => setFront(!front)}
-    //   >
-    //     <img src={`/svg_playing_cards/backs/red.svg`} alt="Card Back" style={{ width: '100%', height: 'auto' }} />
-    //   </Card>
-    // </ReactCardFlip>
-    <></>
-  )
+    <Card
+      styles={{
+        body: {
+          padding: 0,
+        }
+      }}
+      hoverable
+    >
+      <img 
+        src={`/svg_playing_cards/fronts/${suit}_${rank}.svg`} 
+        alt={`${rank} of ${suit}`} 
+        style={{ width: '100%', height: 'auto' }} 
+      />
+    </Card>
+  );
 }
+
